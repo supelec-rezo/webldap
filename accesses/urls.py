162c136cdfa7b7cb8074f6ebaed06eefa0f9794c
TsCl <pmontagnier@gmail.com>
2013-07-06 00:41:36 +0200

    Fixed accesses urls

--- accesses/urls.py
+++ accesses/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('accesses.views',
     # Profiles
     url(r'^accesses/$', 'index'),
-    url(r'^accesses/(?P<type>(web|server|app))/(?P<name>[a-z0-9]+)/$', 'show')
+    url(r'^accesses/(?P<type>(web|server|app))/(?P<name>[a-z0-9_.-]+)/$', 'show')
 )
